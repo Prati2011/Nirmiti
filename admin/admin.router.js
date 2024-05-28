33adcf8bf35db7108ce3fbc35660b3d24cc5c304
Vishal Suryawanshi <102354559+rookie-engg@users.noreply.github.com>
2024-05-28 19:55:03 +0530

    Initial commit

---
+++ admin/admin.router.js
@@ -0,0 +1,82 @@
+import express from 'express';
+import debug from 'debug';
+import customerRouter from './routes/customer.router.js';
+import guestRouter from './routes/guest.router.js';
+import attendanceRouter from './routes/attendance.router.js';
+import subscriptionRouter from './routes/subscription.router.js';
+import reportsRouter from './routes/reports.router.js';
+
+const d = debug('router:admin');
+
+const adminRouter = express.Router();
+
+adminRouter.get('/', (req, res, next) => {
+  res.render('admin-login', { layout: false });
+});
+
+adminRouter.post('/authenticate', async (req, res, next) => {
+  const { username, password } = req.body;
+  
+  if (username != 'admin') {
+    res.render('admin-login', { layout: false, usernameNotValid: true });
+    return;
+  }
+  
+  if (password != '123') {
+    res.render('admin-login', { layout: false, passwordNotValid: true });
+    return;
+  }
+  
+  req.session.name = 'vishal';
+  req.session.post = 'clerk';
+
+  res.redirect('/admin/attendance');
+});
+
+adminRouter.get('/logout', (req, res) => {
+  req.session.destroy(() => res.redirect('/admin'));
+});
+
+adminRouter.use((req, res, next) => {
+  if (!req.session?.name || !req.session?.post) {
+    req.session.destroy(() => {
+      res.redirect('/admin');
+    });
+    return;
+  }
+  next();
+});
+
+adminRouter.get('/home', (req, res, next) => {
+  res.render('admin-home', {
+    loginname: req.session.name,
+    post: req.session.post,
+    title: 'Home'
+  });
+});
+
+adminRouter.get('/profile', (req, res, next) => {
+  res.render('admin-profile', {
+    loginname: req.session.name,
+    post: req.session.post,
+    title: 'Admin Profile'
+  });
+});
+
+
+// ------- /customer Routes------------
+adminRouter.use('/customer', customerRouter);
+
+// ---------- /guest Routes------------
+adminRouter.use('/guest', guestRouter);
+
+// --------- /attendance --------------
+adminRouter.use('/attendance', attendanceRouter);
+
+// --------- /subscription ------------
+adminRouter.use('/subscription', subscriptionRouter);
+
+// -------- /reports ---------------
+adminRouter.use('/reports', reportsRouter);
+
+export default adminRouter;
---
+++ insertDummyData.js
@@ -0,0 +1,71 @@
+import { faker } from '@faker-js/faker/locale/en_IND'
+import { models } from './database/models.js';
+import dbInstance from './database/connect.js';
+import { DateTime } from 'luxon';
+
+export async function insertDummyData(run = 5) {
+
+  const customers = [];
+  const guests = [];
+
+  for (let x = 0; x < run; x++) {
+    const messPlan = faker.datatype.number({ max: 30, min: 12 });
+    const amount = messPlan * 40;
+    const paidAmount = faker.helpers.arrayElement([amount, faker.datatype.number({ min: 0, max: amount })]);
+    const online = faker.datatype.number({ min: 0, max: paidAmount });
+    const cash = paidAmount - online;
+    customers.push({
+      ug_number: `UG` + `${x + 1}`.padStart(4, '0'),
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      guardian_firstname: faker.name.firstName(),
+      guardian_surname: faker.name.lastName(),
+      joining_date: faker.date.recent(),
+      Contact: {
+        address: faker.address.streetAddress() + '\n' + faker.address.secondaryAddress() + '\n' + faker.address.streetAddress(),
+        email: faker.internet.email(),
+        phone: faker.phone.number('##########'),
+        guardian_phone: faker.phone.number('##########'),
+      },
+      Subscription: {
+        number_of_days: 30,
+        renew_date: faker.date.past(),
+        remaining_days: faker.datatype.number({ min: 1, max: 30 }),
+        sub_type: faker.helpers.arrayElement(['both', 'both']),
+        status: 'active',
+      },
+      Activity: {
+        last_active: faker.helpers.arrayElement([null]),//, DateTime.now().toISODate()]),
+        last_count: faker.helpers.arrayElement([null]),
+      },
+      Payments: new Array(5).fill(null).map(e => {
+        return {
+          bill_number: faker.helpers.unique(faker.datatype.number),
+          total_amount: amount,
+          paid_amount: paidAmount,
+          cash: online,
+          status: amount == paidAmount ? 'complete' : 'pending',
+          online: paidAmount,
+          cash,
+          payment_date: faker.date.recent(),
+          txnid: faker.random.alphaNumeric(10),
+        }
+      }),
+    });
+
+    guests.push({
+      name: faker.name.fullName(),
+      payamount: 60,
+      paydate: faker.date.recent(),
+      paymode: faker.helpers.arrayElement(['online', 'cash']),
+    });
+  }
+
+  await models.customer.bulkCreate(customers, {
+    include: [models.contact, models.payment, models.subscription, models.activity]
+  });
+
+  await models.guest.bulkCreate(guests);
+
+  console.log('Successfully inserted');
+}
